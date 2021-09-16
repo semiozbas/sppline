d946a3e8a25c2fddbbaeb3500cd95f1652c31072
semi <semi.ozbas@kartaca.com>
2021-09-16 23:11:54 +0300

    k8s-deploy

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ class TestHello(unittest.TestCase):
     def test_hello_hello(self):
         rv = self.app.get('/hello/')
         self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'Hello World!\n')
+        self.assertEqual(rv.data, b'Hello kartaca !\n')
 
     def test_hello_name(self):
         name = 'Simon'
